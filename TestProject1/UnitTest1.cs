3228e0b05e5fec5e1916635ad69c87998f5ab03e
KD.Livadnyi <livadny.810@gmail.com>
2025-03-05 14:11:28 +0300

    •	Разработка тестов для выражений, содержащих три операнда и две операции

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -63,5 +63,69 @@ namespace TestProject1
             //Assert
             Assert.Equal(expected, actual);
         }
+
+        [Theory]
+        [InlineData("- + 7 2 3")]
+        [InlineData("- 2 + 5 3")]
+        public void GIVEN_three_positive_numbers_WHEN_operation_plus_RETURN_positive(string input)
+        {
+            //Arrange
+            int actual, expected = 6;
+            Calc c = new Calc();
+
+            // Act
+            actual = c.calculate(input);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("+ - 7 7 2")]
+        [InlineData("- 3 + 2 3")]
+        public void GIVEN_three_positive_numbers_WHEN_operation_minus_RETURN_positive(string input)
+        {
+            //Arrange
+            int actual, expected = 2;
+            Calc c = new Calc();
+
+            // Act
+            actual = c.calculate(input);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("- * 7 2 4")]
+        [InlineData("* 2 + 2 3")]
+        public void GIVEN_three_positive_numbers_WHEN_operation_multiply_RETURN_positive(string input)
+        {
+            //Arrange
+            int actual, expected = 10;
+            Calc c = new Calc();
+
+            // Act
+            actual = c.calculate(input);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("- / 8 2 4")]
+        [InlineData("- 2 / 10 5")]
+        public void GIVEN_three_positive_numbers_WHEN_operation_division_RETURN_positive(string input)
+        {
+            //Arrange
+            int actual, expected = 0;
+            Calc c = new Calc();
+
+            // Act
+            actual = c.calculate(input);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
     }
 }
